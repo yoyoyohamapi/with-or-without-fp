338226c9ac260f88cdabd8d4a72d29467b6a049c
吴晓军 <woo@softshot.local>
2017-03-30 22:01:59 +0800

    functors

--- fp.js
+++ fp.js
@@ -73,6 +73,174 @@ const filter = curry(function (accumulator, array) {
     return array.filter(accumulator);
 });
 
+//////////////// Functors //////////////////////
+// Identity
+const Identity = function (x) {
+    this.__value = x;
+};
+
+Identity.of = function (x) { return new Identity(x); };
+
+Identity.prototype.map = function (f) {
+    return Identity.of(f(this.__value));
+};
+
+Identity.prototype.inspect = function () {
+    return 'Identity(' + inspect(this.__value) + ')';
+};
+
+// Maybe
+const Maybe = function (v) {
+    this.__value = v;
+}
+
+Maybe.of = function (v) {
+    return new Maybe(v);
+}
+
+Maybe.prototype.isNothing = function () {
+    return this.__value === null || this.value === undefined;
+}
+
+Maybe.prototype.map = function (f) {
+    return this.isNothing() ? Maybe.of(null) : Maybe.of(f(this.__value));
+}
+
+Maybe.prototype.join = function () {
+    return this.isNothing() ? Maybe.of(null) : this.__value;
+}
+
+Maybe.prototype.chain = function (f) {
+    return this.map(f).join();
+}
+
+Maybe.prototype.ap = function (other) {
+    return this.isNothing() ? Maybe.of(null) : other.map(this.__value);
+}
+
+Maybe.prototype.inspect = function () {
+    return 'Maybe(' + inspect(this.__value) + ')';
+}
+
+// Left
+const Left = function (v) {
+    this.__value = v;
+}
+
+Left.of = function (v) {
+    return new Left(v);
+}
+
+Left.prototype.map = function (f) {
+    // Left functor 会短路之后的一切操作
+    return this;
+}
+
+Left.prototype.join = function () {
+    return this;
+}
+
+Left.prototype.chain = function (f) {
+    return this;
+}
+
+Left.prototype.ap = function (other) {
+    return this;
+}
+
+Left.prototype.inspect = function () {
+    return 'Left(' + inspect(this.__value) + ')';
+}
+
+// Right
+const Right = function (v) {
+    this.__value = v;
+}
+
+Right.of = function (v) {
+    return new Right(v);
+}
+
+Right.prototype.map = function (f) {
+    return Right.of(f(this.__value));
+}
+
+Right.prototype.join = function () {
+    return this.__value;
+}
+
+Right.prototype.chain = function (f) {
+    return this.map(f).join();
+}
+
+Right.prototype.ap = function (other) {
+    return other.map(this.__value);
+}
+
+Right.prototype.inspect = function () {
+    return 'Right(' + inspect(this.__value) + ')';
+}
+
+// Either
+Either = function () {}
+
+Either.of = function (v) {
+    return Right.of(v);
+}
+
+// IO
+const IO = function (f) {
+    this.unsafePerformIO = f;
+}
+
+IO.prototype.of = function (v) {
+    return new IO(function () {
+        return v;
+    });
+}
+
+IO.prototype.map = function (f) {
+    return new IO(compose(f, this.unsafePerformIO));
+}
+
+IO.prototype.join = function () {
+    return this.unsafePerformIO();
+}
+
+IO.prototype.chain = function (f) {
+    return this.map(f).join();
+}
+
+IO.prototype.ap = function (other) {
+    return other.map(this.unsafePerformIO)
+}
+
+IO.prototype.inspect = function () {
+    return 'IO(' + inspect(this.unsafePerformIO) + ')';
+}
+
+///// Functor Helper //////////////
+const fmap = curry(function (f, m) {
+    return m.map(f);
+});
+
+const fchain = curry(function (f, m) {
+    return m.chain(f);
+});
+
+const feither = curry(function (f, g, e) {
+    switch(e.constructor) {
+        case Left:
+            return f(e.__value);
+        case Right:
+            return g(e.__value);
+    }
+});
+
+inspect = function (x) {
+    return (x && x.inspect) ? x.inspect() : x;
+};
+
 module.exports = {
     placeholder,
     curry,
@@ -82,5 +250,14 @@ module.exports = {
     identity,
     map,
     reduce,
-    filter
+    filter,
+    Identity,
+    fmap,
+    fchain,
+    either,
+    Maybe,
+    Left,
+    Right,
+    Either,
+    IO
 };
